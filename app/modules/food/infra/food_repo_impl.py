f3483bf93e0589053d350ff142206736a5f7ddbf
iamjunhyeong <dddodo447@gmail.com>
2025-05-30 03:06:07 +0900

    feat: food-data 넣기 (#56) 엑셀 파일에 있는 데이터를 DB로 옮긴다

--- app/modules/food/infra/food_repo_impl.py
+++ app/modules/food/infra/food_repo_impl.py
@@ -1,9 +1,14 @@
 from abc import ABC
+
+from sqlalchemy.exc import IntegrityError
+
 from app.database import SessionLocal
 from app.modules.food.domain.repository.food_repo import IFoodRepository
 from app.modules.food.domain.food import Food as FoodVO
 from app.modules.food.infra.db_models.food import Food
 from app.utils.db_utils import row_to_dict
+from app.utils.exceptions.error_code import ErrorCode
+from app.utils.exceptions.handlers import raise_error
 
 
 class FoodRepository(IFoodRepository, ABC):
@@ -18,3 +23,17 @@ class FoodRepository(IFoodRepository, ABC):
             if food is None:
                 return None
             return FoodVO(**row_to_dict(food))
+
+    def insert_food_data(self, food_vo_list):
+        with SessionLocal() as db:
+            food_list = []
+            for food_vo in food_vo_list:
+                food_list.append(Food(
+                    **food_vo.__dict__
+                ))
+            try:
+                db.bulk_save_objects(food_list)
+                db.commit()
+            except IntegrityError:
+                db.rollback()
+                raise raise_error(ErrorCode.FOOD_EXISTS)
